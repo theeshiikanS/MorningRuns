489da6861845dde0e44f67a160d0ab3ea14af6a9
theeshiikanS <s.theeshiikan@gmail.com>
2026-06-09 10:18:03 -0400

    Initialize BALL_RUN_DATA with player info
    Added initial player data and history for the team.

---
+++ data.js
@@ -0,0 +1,23 @@
+// ==========================================
+// MASTER TEAM SPREADSHEET DATA
+// ==========================================
+var BALL_RUN_DATA = {
+    players: [
+        // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
+        ["Kayanan", "Carry+", "0-0", "6'1\"", "180 lbs", "6'3\"", ""],
+        ["Merushan", "Mid", "0-0", "--", "--", "--", ""],
+        ["Theesik", "Carry+", "0-0", "--", "--", "--", ""],
+        ["Royzen", "Body-", "0-0", "--", "--", "--", ""],
+        ["Vinni", "Body", "0-0", "--", "--", "--", ""],
+        ["Divjot", "Body", "0-0", "5'2", "--", "--", ""],
+        ["Shaj", "Chuckels", "0-0", "--", "--", "--", ""],
+        ["Ayaan", "yn-", "0-0", "--", "--", "--", ""],
+        ["Sharun", "mid", "0-0", "--", "--", "--", ""],
+        ["Lakzen", "Liability+", "0-0", "--", "--", "--", ""],
+        ["Ak", "Mid", "0-0", "--", "--", "--", ""],
+    ],
+    history: [
+        // Format: { desc: "What changed", date: "Date", author: "Who", reviewers: "Who reviewed" }
+        { desc: "Initial release", date: "6/5/2026", author: "KM", reviewers: "CB, SR" }
+    ]
+};
